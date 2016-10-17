75f93d57580393174a4dbb97040ebe02b2f5d1be
Temaol <Temhikol@gmail.com>
2016-10-17 22:30:50 +0300

    add aquarium

--- class9/felichita.js
+++ class9/felichita.js
@@ -2,7 +2,7 @@ function felichita() {
     var text = '';
     for (n = 1000; n <= 9999; n++) {
         nn = n.toString();
-        if (nn[0] + nn[1] == nn[2] + nn[3] || nn[1] + nn[0] == nn[2] + nn[3]) {
+        if (Number(nn[0]) + Number(nn[1]) == Number(nn[2]) + Number(nn[3])) {
         text += nn + "\n"};
     }
   document.getElementById('felres').innerHTML = text;
--- class9/kuby.js
+++ class9/kuby.js
@@ -2,9 +2,18 @@ function kuby(x) {
     [d, a, b, c] = x.split(' ');
     var otvet = div(a, d) * div(b, d) * div(c, d);
     // console.log(otvet);
-    document.getElementById('otvet').innerHTML = otvet;
+    return(otvet);
 }
 
 function div(val, by) {
     return (val - val % by) / by;
 }
+
+// tests
+if (kuby('1 3 3 3') == 27) {
+    console.log('Test for 1 3 3 3 passed, otvet is: ' + kuby('1 3 3 3'));
+}
+           if (kuby('2 4 4 8') == 16) {
+    console.log('Test for 2 4 4 8 passed, otvet is: ' + kuby('2 4 4 8'));
+}
+
